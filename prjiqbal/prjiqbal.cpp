1cb13d7cf0c8e1a85f3db19d4deb2176f1aafdd8
iqbal232021 <furqoniqbal472@gmail.com>
2024-03-14 18:05:46 +0700

    memperbaiki int  main

--- prjiqbal/prjiqbal.cpp
+++ prjiqbal/prjiqbal.cpp
@@ -1,15 +1,13 @@
-// prjiqbal.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 using namespace std;
+
 int arr[9];
 int n;
 
 void input() {
 	while (true) {
 		cout << "Masukkan banyaknya elemen pada array : ";
-		
+		cin >> n;
 		if (n <= 20)
 			break;
 		else {
@@ -27,58 +25,48 @@ void input() {
 	}
 }
 
-void insertionSort() {
+void InsertionSort() {
 	int i, temp, j;
 
-	for (i = 1; i <= n - 1; i++) { //step1
-		temp = arr[i]; //step 2
-		j = i; //step3
-		while (j >= 0 && arr[j] > temp) //step 4
+	for (i = 1; i <= n - 1; i++) {
+
+		temp = arr[i];
+
+		j = i - 1;
+
+		while (j >= 0 && arr[j] > temp)
 		{
-			arr[j + 1] = arr[j]; //step 4a
-			j--; //step 4b
+			arr[j + 1] = arr[j];
+			j--;
 		}
-		arr[j + 1] = temp; //step 5
-
+		arr[j + 1] = temp;
 	}
-
-void display(); {
-		cout << endl;
-		cout << "====================" << endl;
-		cout << "Element Array yang telah tersusun" << endl;
-		cout << "====================" << endl;
-		cout << endl;
-		for (int j = 0; j < n; j++) {
-			cout << arr[j];
-			if (j < n - 1) {
-				cout << " --> ";
-			}
-		}
-		cout << endl;
-		cout << endl;
-		cout << "Jumlah pass = " << n - 1 << endl;
-		cout << endl;
 }
-	
 
+void display() {
+	cout << endl;
+	cout << "====================" << endl;
+	cout << "Element Array yang telah tersusun" << endl;
+	cout << "====================" << endl;
+	cout << endl;
+	for (int j = 0; j < n; j++) {
+		cout << arr[j];
+		if (j < n - 1) {
+			cout << " --> ";
+		}
+	}
+	cout << endl;
+	cout << endl;
+	cout << "Jumlah pass = " << n - 1 << endl;
+	cout << endl;
+}
 
-
-int main() {
+int main()
+{
 	input();
-	insertionSort();
+	InsertionSort();
 	display();
 
 	system("pause");
 	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
